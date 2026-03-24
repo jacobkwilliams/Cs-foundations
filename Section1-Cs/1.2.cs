224f13cff09dda65dcd5752c4ab0f370cd703bd2
Jacob Williams <jkwilliamshome155@gmail.com>
2026-03-24 13:19:58 -0500

    Initial commit - Section 1 excercises from Microsoft C# Cert.

---
+++ Section1-Cs/1.2.cs
@@ -0,0 +1,27 @@
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+
+Console.WriteLine("Hello \"World\"!");
+
+Console.WriteLine("c:\\source\\repos\n");
+
+Console.WriteLine("Generating invoices for customer \"Constoso Corp\"...\n");
+Console.WriteLine("invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!\n");
+Console.Write("Output Directory:\t");
+Console.Write(@"c:\invoices");
+
+Console.Write("");
+
+
+
+
+Console.WriteLine("\n");
+Console.WriteLine(@"     c:\source\repos
+        (this is where your code goes)");
+
+Console.WriteLine("\n");
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+
+
